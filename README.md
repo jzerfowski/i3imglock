cfce5abd896c45fe91599950de1701a9c1c7b90a
Jan Zerfowski <github@janzerfowski.de>
2020-04-26 21:49:31 +0200

    Renaming from suspendlock to i3imglock, cleaning up the git, going from .png to .jpg as default file format

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-# Automatic locking in i3
+# Automatic locking in i3 with i3imglock
 
 This is an alternative script to [blurlock](https://github.com/manjaro/packages-community/blob/master/i3/i3exit/blurlock) (default in manjaro with i3), written in python, that randomly chooses an image file from folder './images/' and displays it as the background of i3lock.
-The [suspendlock.service](./suspendlock.service) file can be used as an example to be executed automatically at suspend.
+The [i3imglock.service](i3imglock.service) file can be used as an example to be executed automatically at suspend.
 
-Source of the [example image](./images/example.png): Jan Zerfowski, Bolivia, Madidi National Park
+It can take several command-line arguments for configuration:
+- `--images_directory` to choose the directory of the images
+- `--logging_level` to set a loglevel
+- `--filetypes` to choose which filetypes we sample from
+- `--i3lock_options` to set options that are passed to on to `i3lock`
+- `--i3lock_fallback_command` to set which command should be executed in case of an error
+
+All arguments can be shown by `i3imglock --help`
+
+## Requirements
+- [NumPy](https://numpy.org/)
+- [PIL](https://pypi.org/project/Pillow/)
+- [screeninfo](https://pypi.org/project/screeninfo/)
+
+Source of the [example image](./images/example.jpg): Jan Zerfowski, Bolivia, Madidi National Park
 
 #### Example
-![suspendlock in action](readme_example_image.png "Example usage")
+![i3imglock in action](images/example.jpg "Example usage")
 
 ## What it can do
 - Display random image from folder
@@ -16,14 +30,14 @@ Source of the [example image](./images/example.png): Jan Zerfowski, Bolivia, Mad
 
 ## Installation
 - Copy image files in a compatible format to [./images/](./images/) (or any other directory)
-- Make suspendlock.py executable
-    - `chmod u+x suspendlock.py`
+- Make i3imglock.py executable
+    - `chmod u+x i3imglock.py`
 - Create a symbolic link in your $PATH
-    - `ln -s <directory of suspendlock.py> /usr/local/bin/suspendlock`
-- Copy the [suspendlock.service](./suspendlock.service) file to a folder which systemd cares about
-    - `cp suspendlock.service /etc/systemd/system/suspendlock.service`
+    - `ln -s <directory of i3imglock.py> /usr/local/bin/i3imglock`
+- Copy the [i3imglock.service](./i3imglock.service) file to a folder which systemd cares about
+    - `cp i3imglock.service /etc/systemd/system/i3imglock.service`
 - To apply custom settings, use arguments when calling
-    - `suspendlock.py -h` 
+    - `i3imglock.py -h` 
 
 ## Issues
 - Needs images in exactly 16:9 ratio (or whatever implicit ratio is given by settings['screen_resolution'])
---
+++ i3imglock.py
@@ -0,0 +1,106 @@
+#! /usr/bin/python
+
+import os
+import logging
+
+import numpy as np
+import subprocess
+import argparse
+import screeninfo
+from PIL import Image
+
+logging_levels = {'debug': logging.DEBUG, 'warning': logging.WARNING, 'error': logging.ERROR}
+images_directory_default = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images/')
+filetypes_default = ['jpg', 'png']
+i3lock_options_default = "--show-failed-attempts"
+i3lock_fallback_command_default = "i3lock"
+
+parser = argparse.ArgumentParser(
+    description='i3imglock.py to call i3lock with custom images and options. Requires numpy, PIL and screeninfo')
+
+parser.add_argument('--images_directory', type=str, default=images_directory_default, required=False,
+                    help=f"Absolute directory to images which can be used for i3lock "
+                         f"(default: {str(images_directory_default)})")
+
+parser.add_argument('--logging_level', type=str, default='warning', required=False, choices=logging_levels,
+                    help=f"Logging level "
+                         f"(default: warning)")
+
+parser.add_argument('--filetypes', nargs='+', required=False, default=filetypes_default,
+                    help=f"Set filetypes "
+                         f"(default: {filetypes_default})")
+
+parser.add_argument('--i3lock_options', type=str, default=i3lock_options_default,
+                    help=f'Arguments which are passed on to i3lock '
+                         f'(default: --i3lock_options="{i3lock_options_default}")')
+
+parser.add_argument('--i3lock_fallback_command', type=str, default=i3lock_fallback_command_default,
+                    help=f'Fallback command if any of the custom script throws an exception. '
+                         f'(default: --i3lock_fallback_command="{i3lock_fallback_command_default}")')
+
+settings = parser.parse_args()
+
+
+def filetype_filter(filename, filetypes):
+    # Return if the filetype of filename is contained filetypes
+    return os.path.splitext(filename)[1].endswith(tuple(filetypes))
+
+
+def get_images(image_directory, filetype_filter, filetypes):
+    # Get all images that are in image_directory and filter them by using function filetype_filter
+    images = [image.path for image in os.scandir(image_directory) if filetype_filter(image.path, filetypes)]
+    images = [os.path.join(image_directory, image) for image in images]
+
+    return images
+
+
+logging.basicConfig(level=logging_levels[settings.logging_level])
+
+# images_directory = os.path.join(settings['script_dir'], settings['backgrounds_relative_dir'])
+images_directory = settings.images_directory
+
+# Get images from background images directory
+image_paths = get_images(images_directory, filetype_filter=filetype_filter, filetypes=settings.filetypes)
+logging.debug(f"Found {len(image_paths)} images in {images_directory}")
+
+# Figure out the total resolution of the screen
+monitors = screeninfo.get_monitors()
+
+height = max(monitor.y + monitor.height for monitor in monitors)
+width = max(monitor.x + monitor.width for monitor in monitors)
+
+# Set the command for the locking
+i3lock_command = ['i3lock'] + settings.i3lock_options.split(' ') + ['--raw', f"{width}x{height}" + ':rgb', '--image',
+                                                                    '/dev/stdin']
+
+# Create an empty canvas the size of the entire screen
+# Some areas might be left empty (i.e., black), if the screens are not the same sizes
+canvas = Image.new(mode='RGB', size=(width, height))
+logging.debug(f"Creating canvas with {width}x{height}")
+
+# Choose an independent image for each monitor and paste them onto the canvas at the position of the monitor
+image_paths_selected = np.random.choice(image_paths, size=len(monitors), replace=False)
+logging.debug(f"Selected {len(image_paths_selected)} image: {image_paths_selected}")
+
+# If size ratios do not match the monitor's ratio, the might currently still get squeezed against their ratio
+for monitor, image_path in zip(monitors, image_paths_selected):
+    # Choose a random image from available images
+    # image_path = np.random.choice(image_paths)
+    image = Image.open(image_path)
+    image = image.resize(size=(monitor.width, monitor.height))
+    canvas.paste(image, box=(monitor.x, monitor.y, monitor.x + monitor.width, monitor.y + monitor.height))
+
+try:
+    logging.debug(f"Calling locker: {' '.join(i3lock_command)} with our canvas as input")
+    i3lock_subprocess = subprocess.Popen(i3lock_command, stdin=subprocess.PIPE)
+    i3lock_communicate = i3lock_subprocess.communicate(input=canvas.tobytes())
+
+    logging.debug(f"i3lock_subprocess.communicate(): {i3lock_communicate}")
+    logging.debug(f"i3lock_subprocess stdout: {i3lock_subprocess.stdout}")
+
+except Exception as e:
+    logging.error("Error occured in lockscript. Locking with default i3lock")
+    logging.error(e)
+
+    logging.warning(f"Calling i3lock default: {' '.join(settings.i3lock_fallback_command)}")
+    subprocess.call(settings.i3lock_fallback_command)
